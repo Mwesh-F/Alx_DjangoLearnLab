fe0cb1e66afcb52b400fcec00a4f80417d94b2ba
Frida Mwende <mwendehfrida@gmail.com>
2025-08-03 21:14:39 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,3 +124,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
